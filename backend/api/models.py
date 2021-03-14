1b59f6e9b0d85cda241595fe7b370966e3ad51eb
Your Name <you@example.com>
2021-03-13 17:04:45 -0800

    create display and add data endpoints

---
+++ backend/api/models.py
@@ -0,0 +1,6 @@
+from . import db 
+
+class Data(db.Model):  # data for websites and passwords
+    id = db.Column(db.Integer, primary_key=True)
+    website = db.Column(db.String(100))  # website column
+    password = db.Column(db.String(100))  # password for associated website
